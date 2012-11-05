33e3602dc904865fd634864e8d7d42fdef15eaef
U-christoph303-HP\christoph303 <christoph303@christoph303-HP.(none)>
2012-11-05 13:09:08 +0100

    Initial commit.

---
+++ Types.h
@@ -0,0 +1,119 @@
+/*
+ *  Grundfos GENIBus Library.
+ *
+ *  (C) 2007-2012 by Christoph Schueler <cpu12.gems@googlemail.com>
+ *
+ *   All Rights Reserved
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ */
+
+/* http://www.opengroup.org/onlinepubs/000095399/basedefs/sys/types.h.html */
+
+#if !defined(__TYPES_H)
+#define __TYPES_H
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif  /* __cplusplus */
+
+
+/* check for C99-Compiler */
+#if defined(__STDC_VERSION__)
+    #if __STDC_VERSION__ >= 199901L
+        #define C99_COMPILER
+    #endif
+#endif
+
+/* check for C++-Compiler */
+#if defined(__cplusplus)
+    #define CPP_COMPILER
+#endif
+
+#if (defined(__IAR_SYSTEMS_ICC__) && defined(_DLIB_ADD_C99_SYMBOLS)) || (defined(_C99_COMPILER)) || (defined(_CPP_COMPILER))
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef unsigned char boolean;
+
+typedef /*@signed-integral-type@*/ int8_t       sint8;
+typedef /*@unsigned-integral-type@*/ uint8_t    uint8;
+typedef /*@signed-integral-type@*/ int16_t      sint16;
+typedef /*@unsigned-integral-type@*/ uint16_t   uint16;
+typedef /*@signed-integral-type@*/ int32_t      sint32;
+typedef /*@unsigned-integral-type@*/ uint32_t   uint32;
+
+typedef /*@signed-integral-type@*/ int_least8_t     sint8_least;
+typedef /*@unsigned-integral-type@*/ uint_least8_t  uint8_least;
+typedef /*@signed-integral-type@*/ int_least16_t    sint16_least;
+typedef /*@unsigned-integral-type@*/ uint_least16_t uint16_least;
+typedef /*@signed-integral-type@*/ int_least32_t    sint32_least;
+typedef /*@unsigned-integral-type@*/ uint_least32_t uint32_least;
+
+#if !defined(TRUE)
+    #define TRUE    ((boolean)true)
+#endif
+
+#if !defined(FALSE)
+    #define FALSE   ((boolean)false)
+#endif
+
+#else
+/* Types  */
+typedef unsigned char   boolean;
+typedef signed char     sint8;
+typedef unsigned char   uint8;
+typedef signed short    sint16;
+typedef unsigned short  uint16;
+typedef signed long     sint32;
+typedef unsigned long   uint32;
+
+typedef signed char     sint8_least;
+typedef unsigned char   uint8_least;
+typedef signed short    sint16_least;
+typedef unsigned short  uint16_least;
+typedef signed long     sint32_least;
+typedef unsigned long   uint32_least;
+
+#if !defined(TRUE)
+    #define TRUE    ((boolean)1)
+#endif
+
+#if !defined(FALSE)
+    #define FALSE   ((boolean)0)
+#endif
+
+#endif
+
+#if !defined(NULL)
+#define NULL        0
+#endif
+
+typedef float           float32;
+typedef double          float64;
+typedef void *          pvoid;
+typedef unsigned int    SizeType;
+typedef int             PtrDiffType;
+
+#define ARRAY_SIZE(a)	(sizeof((a)) / sizeof((a[0])))
+
+
+#if defined(__cplusplus)
+}
+#endif  /* __cplusplus */
+
+#endif /* __TYPES_H */
---
+++ tests/Crc_Test/Crc_Test/Crc_Test.ino
@@ -0,0 +1,93 @@
+
+#include <Genibus.h>
+
+/*
+**
+**  Known-good telegrams and CRCs.
+**
+*/
+uint8 TestVector0[] = {
+    0x27, 0x07, 0x20, 0x01, 0x02, 0xC3, 0x02, 0x10, 0x1A
+};
+
+uint8 TestVector1[] = {
+    0x27, 0x0e, 0xfe, 0x01, 0x00, 0x02, 0x02, 0x03, 0x04, 0x02, 0x2e, 0x2f, 0x02, 0x02, 0x94, 0x95
+};
+
+uint8 TestVector2[] = {
+  0x24, 0x0e, 0x01, 0x20, 0x00, 0x02, 0x46, 0x0e, 0x04, 0x02, 0x20, 0xf7, 0x02, 0x02, 0x03, 0x01
+};
+
+uint8 TestVector3[] = {
+  0x24, 0x10, 0x01, 0x20, 0x02, 0x0c, 0x82, 0x3e, 0x00, 0x39, 0x82, 0x15, 0x00, 0x64, 0x82, 0x09, 0x00, 0xfa
+};
+
+uint8 TestVector4[] = {
+  0x27, 0x0f, 0x20, 0x01, 0x02, 0x04, 0x02, 0x10, 0x1a, 0x1b, 0x04, 0x02, 0x04, 0x05, 0x03, 0x81, 0x06
+};
+
+uint8 TestVector5[] = {
+  0x24, 0x0e, 0x01, 0x20, 0x02, 0x04, 0x7a, 0x42, 0x39, 0x80, 0x04, 0x02, 0xb5, 0xc8, 0x03, 0x00
+};
+
+uint16 ExpectedResults[] = {
+  0x901c,
+  0xa2aa,
+  0x0004,
+  0x910a,
+  0x802a,
+  0xf2d7
+};
+
+/*
+**  Calculate the CRC of a GENIBus telegram (excluding start-delimiter).
+*/
+uint16 calculateCRC(uint8 const * array, uint16 len)
+{
+  uint16 idx;
+  
+  Crc_Init(0xffffu);
+  for (idx = (uint16)1u; idx < len; ++idx) {
+    Crc_Update(array[idx]);
+  }
+  return Crc_Get();
+}
+
+void resultOK(uint16 value, uint16 expected)
+{
+   if (value == expected) {
+      Serial.println("OK");
+   } else {
+      Serial.println("Invalid CRC!!!");
+   } 
+}
+
+void setup(void)
+{
+  Serial.begin(9600); 
+}
+
+void loop(void)
+{
+   uint16 result;
+   
+   result = calculateCRC(TestVector0, ARRAY_SIZE(TestVector0));
+   resultOK(result, ExpectedResults[0]);
+   
+   result = calculateCRC(TestVector1, ARRAY_SIZE(TestVector1));
+   resultOK(result, ExpectedResults[1]);
+   
+   result = calculateCRC(TestVector2, ARRAY_SIZE(TestVector2));
+   resultOK(result, ExpectedResults[2]);
+   
+   result = calculateCRC(TestVector3, ARRAY_SIZE(TestVector3));
+   resultOK(result, ExpectedResults[3]);   
+   
+   result = calculateCRC(TestVector4, ARRAY_SIZE(TestVector4));
+   resultOK(result, ExpectedResults[4]); 
+   
+   result = calculateCRC(TestVector5, ARRAY_SIZE(TestVector5));
+   resultOK(result, ExpectedResults[5]);    
+}
+
+
