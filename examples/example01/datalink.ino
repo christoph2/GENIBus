b8be6438953290f5f5c3f6cbbde83039208dd0ab
Christoph Schueler <cpu12.gems@googlemail.com>
2012-11-10 09:43:42 +0100

    Added connect-request.

--- examples/example01/datalink.ino
+++ examples/example01/datalink.ino
@@ -2,7 +2,26 @@
 static uint8 sendBuffer[0xff];
 static Crc crc(0xffffu);
 
-uint16 sendPDU(uint8 sd, uint8 da, uint8 sa, uint8 * data, uint8 len)
+static const uint8 connectReqPayload[] = {
+  0x00, 0x02, 0x02, 0x03, 0x04, 0x02, 0x2e, 0x2f, 0x02, 0x02, 0x94, 0x95
+};
+
+void dumpPDU(byte len)
+{
+  uint8 idx;
+
+  for (idx = 0; idx < (len + 6); ++idx) {
+     //printf("%#x ", sendBuffer[idx]);   
+     sendBuffer[idx];
+  }
+}
+
+void connectRequest(uint8 sa)
+{
+   sendPDU(0x27, 0xfe, sa, connectReqPayload, ARRAY_SIZE(connectReqPayload)); 
+}
+
+void sendPDU(uint8 sd, uint8 da, uint8 sa, uint8 const * data, uint8 len)
 {
   uint8 idx;
   uint16 calculatedCrc;
@@ -24,74 +43,8 @@ uint16 sendPDU(uint8 sd, uint8 da, uint8 sa, uint8 * data, uint8 len)
   sendBuffer[idx] = HIBYTE(calculatedCrc);
   sendBuffer[idx + 1] = LOBYTE(calculatedCrc);  
   
-
-#if 0
-  res = ReadCOMPort(data, 256, &uNumRead);
-
-  if (res != S_OK) {
-    GetFormattedError(res, msg, 1024);
-    return ERR_WINDOWS;
-  } 
-  else {
-    if (uNumRead == 0) {
-      return ERR_TIMEOUT;
-    }
-
-    if ((CheckCRC(data, uNumRead)) == TRUE) {
-      if (data[0] == SD_DATA_REPLY) {
-        if (source_address == data[2]) {
-          len = data[1];
-
-          if (destination_address == ADDR_CONNECT_REQU) {
-            connect_addr = data[3]; /* Connect-Request. */
-          }
-
-          idx    = 4;
-          ao_i   = 0; /* ADPUs_out_idx. */
-
-          while (idx < len + 1) {
-            adpu_len   = (data[idx + 1] & 0x3f);
-            adpu_class = data[idx];
-
-            for (i = 0; i < numpdus_in; i++) {    /* Prüfen, ob die ADPU angefordert wurde. */
-              if ((HIBYTE(adpus_in[i].adpu_head)) == adpu_class) {
-                adpus_out[ao_i].adpu_head = MAKEWORD(data[idx], data[idx + 1]);
-
-                if (adpu_class == CLASS_ASCII_STRINGS) {
-                  strncpy((char *)adpus_out[ao_i].adpu_data,
-                  (char *)&data[idx + 2], MAX_ASCII_LEN / 2);
-                } 
-                else if (adpu_class == CLASS_COMMANDS) {
-                } 
-                else {
-                  if (adpu_len > 0) {             /* Daten entsprechend der Länge kopieren. */
-                    uint8 t;
-                    t = MIN(adpu_len, LOBYTE(   /* schützt vor Buffer-Overflows. */
-                    adpus_out[ao_i].adpu_head));
-                    memcpy((void *)adpus_out[ao_i].adpu_data, (void *)&data[idx + 2], t);
-                  }
-                }
-
-                break;
-              }
-            }
-
-            idx += (adpu_len + 2);
-          }
-        } 
-        else {
-          return ERR_WRONG_ADDR;
-        }
-      } 
-      else {
-        return ERR_FRAMING;
-      }
-    } 
-    else {
-      return ERR_CRC;
-    }
+  for (idx = 0; idx < (len + ((uint8)0x06)); ++idx) {
+     Serial.write(sendBuffer[idx]);
   }
-#endif
-  //Sleep(50);
 }
 
--- examples/example01/example01.ino
+++ examples/example01/example01.ino
@@ -3,15 +3,22 @@
 #include <EthernetUdp.h>
 #include <Genibus.h>
 
+#define GB_MASTER_ADDRESS  0x01
+
 byte macAddress[] = { 0xde, 0xad, 0xaf, 0xfe, 0xaa, 0x55  };
-byte ipAddress[] = {192, 168, 100, 20};
-IPAddress ip(192, 168, 1, 177);
-byte gateway[] = { 192, 168, 100, 1 };
 byte subnet[] = { 255, 255, 255, 0 };
 unsigned int localPort = 6734;
 
+/* TODO: The IP-configuration has to be adjusted to your needs!!! */
+IPAddress ipAddress(192, 168, 100, 20);
+byte gateway[] = { 192, 168, 100, 1 };
+
+#define LED_PIN  13  /* Pin 13 has an LED connected on most Arduino boards, otherwise this should be changed. */
+
 void setup(void)
 {
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   Serial.begin(9600);
   Ethernet.begin(macAddress, ipAddress);
   
@@ -20,7 +27,24 @@ void setup(void)
 
 void loop(void)
 {
+  byte receivedByte;
+  byte byteCount;
+  byte idx;
+  
+  /* We are trying only once to communicate. */
+  connectRequest(GB_MASTER_ADDRESS);
+  while (Serial.available() == 0) {
+    /* Blocking for now. */
+  }
+  /* Well, it seems the pump is talking to us :-) */
+  digitalWrite(LED_PIN, HIGH);
 
+  idx = 0;
+  while (Serial.available() > 0) {
+    receivedByte = Serial.read();
+    ++idx;
+  }
+  
   while (true) {
     /* Endless loop. */
   }
