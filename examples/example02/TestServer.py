5c4f70057800a175a0ef2720503b5d3598d99f48
Christoph Schueler <cpu12.gems@googlemail.com>
2012-11-17 16:27:44 +0100

    Added TestServer.py.

---
+++ examples/example02/TestServer.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import asyncore
+import socket
+import sys
+
+PORT = 6734
+ADDR = (socket.gethostname(), PORT)
+
+class EchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(1024)
+        if data:
+            #print data
+            with file('genibus.log', 'a') as outf:
+                outf.write("%s\n" % data)
+
+
+class EchoServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind(ADDR)
+        self.listen(5)
+        print "TCP-Server [%s:%u] up and running." % ADDR
+
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is None:
+            pass
+        else:
+            sock, addr = pair
+            print '...connected from: %s' % repr(addr)
+            handler = EchoHandler(sock)
+
+    def handle_close(self):
+        print "handle close"
+        self.close()
+
+##
+##    def handle_error(self):
+##        print "handle error"
+##
+
+    def handle_expt(self):
+        print "handle expt"
+
+    def handle_expt_event(self):
+        print "handle expt event"
+
+server = EchoServer(socket.gethostname(), PORT)
+asyncore.loop()
+
+
