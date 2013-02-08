b533b13821d43985cfb9d338f95c416699190fc6
Christoph Schueler <cpu12.gems@googlemail.com>
2013-02-08 15:24:24 +0100

    Added TestClient.py.

---
+++ examples/passThruServer/TestClient.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import socket
+import struct
+import sys
+import time
+
+SERVER = '192.168.100.20'  # TODO: Adjust to the IP-address of your Arduino board!
+
+#SERVER = 'localhost'
+#SERVER = socket.gethostname()
+PORT = 6734
+
+CONNECT_REQ = (
+    0x27,   ##  Start Delimiter
+    0x0e,   ##  Length
+    0xfe,   ##  Destination Address
+    0x01,   ##  Source Address
+            ##
+    0x00,   ##  Class 0: Protocol Data
+    0x02,   ##  OS=0 (GET), Length=2
+    0x02,   ##  df_buf_len
+    0x03,   ##  unit_bus_mode
+    0x04,   ##  Class 4: Configuration Data
+    0x02,   ##  OS=0 (GET), Length=2
+    0x2e,   ##  unit_addr
+    0x2f,   ##  group_addr
+    0x02,   ##  Class 2: Measured Data
+    0x02,   ##  OS=0 (GET), Length=2
+    0x94,   ##  unit_family
+    0x95,   ##  unit_type
+            ##
+    0xa2,   ##  CRC high
+    0xaa    ##  CRC low
+)
+
+socket.setdefaulttimeout(0.5)
+
+def hexDump(data):
+    return [hex(x) for x in data]
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.settimeout(0.5)
+
+    conn = s.connect((SERVER, PORT))
+except Exception as e:
+    msg = "%u: %s -- Press Return to exit." % (e.errno, e.strerror)
+    raw_input(msg)
+    sys.exit(1)
+print "TCP-client up and running."
+
+while True:
+    print "Connect request..."
+    s.send(bytearray(CONNECT_REQ))
+    try:
+      data = s.recv(1024)
+      print "Response: ", hexDump(bytearray(data))
+      print
+    except Exception as e:
+        print "Response: ", str(e)
+    time.sleep(1.0)
+s.close()
+
