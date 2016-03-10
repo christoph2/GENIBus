1183e4979caff78e500f190fc7baf94338f34e11
Christoph Schueler <cpu12.gems@googlemail.com>
2016-03-10 13:23:39 +0000

    Implement configuration tests.

--- tools/tests/testConfiguration.py
+++ tools/tests/testConfiguration.py
@@ -1,16 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
+import unittest
 from genilib.configuration import Config as Config
 
-def main():
-    config = Config("GeniControl")
-    config.load()
-    size = (config.get('window', 'sizex'), config.get('window', 'sizey'))
-    pos = (config.get('window', 'posx'), config.get('window', 'posy'))
-    print("Size: {0}".format(size))
-    print("Position: {0}".format(pos))
-    #config.save()
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.config = Config("GeniControl")
+        self.config.load()
+
+    def getPollingInterval(self):
+        self.assertEqual(self.config.get('general', 'pollinginterval'), 2)
+
+    def testGetSize(self):
+        size = (self.config.get('window', 'sizex'), self.config.get('window', 'sizey'))
+        self.assertEqual(size, (800, 600))
+
+    def testGetPos(self):
+        pos = (self.config.get('window', 'posx'), self.config.get('window', 'posy'))
+        self.assertEqual(pos, (0, 0))
+
+    def testGetSerial(self):
+        self.assertEqual(self.config.get('serial', 'serialport'), '')
+
+    def testGet(self):
+        self.assertEqual(self.config.get('network', 'serverip'), '192.168.100.10')
+
+    def testGet(self):
+        self.assertEqual(self.config.get('network', 'subnetmask'), '255.255.255.0')
+
+    def testGet(self):
+        self.assertEqual(self.config.get('network', 'serverport'), 8080)
+
+    def testGet(self):
+        self.assertEqual(self.config.get('network', 'driver'), 1)
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
 
