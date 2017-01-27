19c8a791d6761abe7c09c4fbad47667a828ab097
Christoph Schueler <cpu12.gems@googlemail.com>
2017-01-27 11:05:14 +0000

    Remove 'PyQt5' dependency for now.

--- setup.py
+++ setup.py
@@ -6,12 +6,7 @@ from glob import glob
 from setuptools import find_packages
 import sys
 
-#VER = sys.version_info.major, sys.version_info.minor
-
-#if VER < (3, 4):
-REQS = ['PyQt5', 'mako', 'pyserial', 'enum']	# 'enum' backport needed.
-#else:
-#    REQS = ['PyQt5', 'mako', 'pyserial']
+REQS = [, 'mako', 'pyserial', 'enum']	# 'PyQt5'
 
 setup(
     name = 'genibus',
